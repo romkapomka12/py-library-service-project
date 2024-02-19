a0fdb730aec4fb9c988a3c5eb275f1688eb434e6
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-02-19 20:06:49 +0200

    modify user and book aplications

--- book/models.py
+++ book/models.py
@@ -5,6 +5,7 @@ class Book(models.Model):
     class CoverChoices(models.TextChoices):
         HARD = "HARD"
         SOFT = "SOFT"
+
     title = models.CharField(max_length=120)
     author = models.CharField(max_length=60)
     cover = models.CharField(max_length=4, choices=CoverChoices.choices)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
