a2fd376a421d2bd15c75f55c3926661d9068bcbf
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-02-27 09:58:59 +0200

    Implemented BookListView with Permission Class

--- book/views.py
+++ book/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from book.models import Book
+from book.permissions import IsAdminOrIfAuthenticatedReadOnly
+from book.serializers import BookSerializer
+
+
+class BookListView(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
