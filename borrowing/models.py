62ab482e69219527753f72f8afff946207d95b2d
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-03-08 09:40:12 +0200

    The presence of copies of the book in the inventory is checked; Also added a pre_save event handler that reduces the number of books in the inventory when creating a new record in the Borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from rest_framework.exceptions import ValidationError
 
 from book.models import Book
 
@@ -9,5 +12,23 @@ class Borrowing(models.Model):
     borrow_date = models.DateTimeField()
     expected_return_date = models.DateTimeField()
     actual_return_date = models.DateTimeField()
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings")
+
+    def __str__(self) -> str:
+        borrow_date = self.borrow_date.strftime("%d-%m-%Y")
+        return f"{self.user} - {self.book} - {borrow_date}"
+
+    def save(self, *args, **kwargs):
+        if self.book.inventory == 0:
+            raise ValidationError('This book is not available for borrowing.')
+        super(Borrowing, self).save(*args, **kwargs)
+
+
+@receiver(pre_save, sender=Borrowing)
+def reduce_inventory_on_borrow(instance, **kwargs):
+    if not instance.pk:  # New instance is being created
+        book = instance.book
+        book.inventory -= 1
+        book.save()
+
