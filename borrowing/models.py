df4f5131cc38f5e45d158552a131e0ab1a92a5d1
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-03-12 18:01:35 +0200

    transfer reduce_inventory_on_borrow o views.py

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
@@ -9,26 +11,16 @@ from book.models import Book
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateTimeField()
-    expected_return_date = models.DateTimeField()
-    actual_return_date = models.DateTimeField()
+
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings"
+    )
 
     def __str__(self) -> str:
         borrow_date = self.borrow_date.strftime("%d-%m-%Y")
         return f"{self.user} - {self.book} - {borrow_date}"
-
-    def save(self, *args, **kwargs):
-        if self.book.inventory == 0:
-            raise ValidationError('This book is not available for borrowing.')
-        super(Borrowing, self).save(*args, **kwargs)
-
-
-@receiver(pre_save, sender=Borrowing)
-def reduce_inventory_on_borrow(instance, **kwargs):
-    if not instance.pk:  # New instance is being created
-        book = instance.book
-        book.inventory -= 1
-        book.save()
 
