6deb4f5e4e62ae7bc3d00922bfc91f1c016046c6
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-03-12 18:10:58 +0200

    created PaymentViewSet

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from book.permissions import IsAdminOrIfAuthenticatedReadOnly
+from payment.models import Payment
+from payment.serializers import PaymentSerializer, PaymentDetailSerializer
+
+
+class PaymentViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+
+    queryset = Payment.objects.all().select_related("borrowing__user")
+    serializer_class = PaymentSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PaymentSerializer
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
