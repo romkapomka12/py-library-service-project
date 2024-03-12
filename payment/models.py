1ee55de3d53acec3db1a9299a411d37272259bef
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2024-03-12 18:08:15 +0200

    added  related_name="payments"

--- payment/models.py
+++ payment/models.py
@@ -14,7 +14,7 @@ class Payment(models.Model):
 
     status = models.CharField(max_length=10, choices=StatusChoices.choices)
     type = models.CharField(max_length=10, choices=TypeChoices.choices)
-    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE, related_name="payments")
     session_url = models.URLField()
     session_id = models.CharField(max_length=50)
     money_to_pay = models.DecimalField(max_digits=7, decimal_places=2)
